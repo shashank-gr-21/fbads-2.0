d384b5fe373020526169588f534ce269e6e795c6
shashank-gr-21 <shashank.br@grorapid.com>
2023-05-25 18:52:22 +0530

    test

---
+++ index.js
@@ -0,0 +1,39 @@
+const dotenv = require("dotenv");
+dotenv.config();
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const bizSdk = require("facebook-nodejs-business-sdk");
+const AdAccount = bizSdk.AdAccount;
+
+const app = express();
+app.use(bodyParser.json());
+
+const access_token = process.env.FB_ACCESS_TOKEN;
+
+const api = bizSdk.FacebookAdsApi.init(access_token);
+
+app.get("/", async (request, response) => {
+  try {
+    const id = "act_100555887474646";
+    const fields = ["name", "objective","daily_budget"];
+    const params = {
+      effective_status: ["ACTIVE", "PAUSED"],
+    };
+    const campaigns = await new AdAccount(id).getCampaigns(fields, params);
+    const data = campaigns.map((item)=>{
+      if (!item._data.daily_budget){
+        return {...item._data,daily_budget:"0"};
+      }
+      return item._data;
+    });
+    response.status(200).send(data)
+  } catch (error) {
+    console.log(error);
+    response.status(400).json(error);
+  }
+});
+
+app.listen(8000,()=>{
+    console.log('Listening on port 8000')
+})
